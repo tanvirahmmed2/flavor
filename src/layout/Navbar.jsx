8ddde58630601c6f66837234fbe6361fbb9a9a31
Tanvir Ahmmed <tanvir004006@gmail.com>
2025-09-19 11:36:47 +0600

    update

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -9,22 +9,25 @@ import { ShopContext } from '../components/Context';
 
 const Navbar = () => {
 
-  const {sidebar, setSidebar}= useContext(ShopContext)
-  
+  const { sidebar, setSidebar } = useContext(ShopContext)
+
   return (
     <nav className='w-full h-16  border-opacity-65 rounded-md flex flex-row items-center justify-around'>
       <div>
-        <a href="/" className='text-2xl font-semibold flex flex-row items-center justify-center gap-2 '><span className='text-orange-500 text-2xl'><MdFoodBank/></span> Flavor</a>
+        <a href="/" className='text-2xl font-semibold flex flex-row items-center justify-center gap-2 '><span className='text-orange-500 text-2xl'><MdFoodBank /></span> Flavor</a>
       </div>
-        <Link to='/' >Home</Link>
-      <div>
+      <div className='w-auto h-16 flex flex-row items-center justify-center gap-2'>
 
+        <Link className='px-4 h-full hover:border-b-2 border-orange-400 flex items-center justify-center' to='/' >Home</Link>
+        <Link className='px-4 h-full hover:border-b-2 border-orange-400 flex items-center justify-center' to='/menu' >Menu</Link>
+        <Link className='px-4 h-full hover:border-b-2 border-orange-400 flex items-center justify-center' to='/order' >Order</Link>
       </div>
 
-      <div>
-        <p className='text-3xl text-orange-500' onClick={()=> setSidebar(!sidebar)}><CgMenuCheese/></p>
+      <div className='w-auto h-16 flex flex-row items-center justify-center gap-4'>
+        <Link className='px-4 p-1 bg-orange-400 text-white rounded-lg' to='/reserve'>Reserve</Link>
+        <p className='text-3xl text-orange-500' onClick={() => setSidebar(!sidebar)}><CgMenuCheese /></p>
       </div>
-      
+
     </nav>
   )
 }
