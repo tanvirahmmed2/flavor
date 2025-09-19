8796961230b14589b0cde19b7ac4dfd7f6940657
Tanvir Ahmmed <tanvir004006@gmail.com>
2025-09-19 11:39:10 +0600

    update

--- src/layout/SideBar.jsx
+++ src/layout/SideBar.jsx
@@ -9,12 +9,17 @@ import { MdFoodBank } from "react-icons/md";
 const SideBar = () => {
   const { sidebar, setSidebar } = useContext(ShopContext)
   return (
-    <div className={`w-[250px] min-h-screen ${sidebar ? "translate-x-0" : "-translate-x-full"} transform transition-transform duration-300 ease-in-out  flex flex-col items-center justify-between py-12 bg-white fixed top-0 z-50 `}>
+    <div className={`w-[250px] select-none min-h-screen ${sidebar ? "translate-x-0" : "-translate-x-full"} transform transition-transform duration-300 ease-in-out  flex flex-col items-center justify-between py-12 bg-white fixed top-0 z-50 `}>
       <div className='w-auto h-auto flex flex-col items-start justify-start gap-4'>
         
         <a href="/" className='text-2xl font-semibold flex flex-row items-center justify-center gap-2 '><span className='text-orange-500 text-2xl'><MdFoodBank /></span> Flavor</a>
 
-        <Link to='/' className='px-4 hover:px-6 '>Home</Link>
+        <div className='w-auto flex flex-col gap-2'>
+          <Link to='/' className='px-4 hover:px-5 '>Home</Link>
+        <Link to='/menu' className='px-4 hover:px-5 '>Menu</Link>
+        <Link to='/order' className='px-4 hover:px-5 '>Order</Link>
+        <Link to='/reserve' className='px-4 hover:px-5 '>Reserve</Link>
+        </div>
 
       </div>
 
