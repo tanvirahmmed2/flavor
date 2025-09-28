54f27d6db83a785c77fbc44e3a69777da7a83d4c
Tanvir Ahmmed <tanvir004006@gmail.com>
2025-09-28 10:47:42 +0600

    update

--- src/components/Food.jsx
+++ src/components/Food.jsx
@@ -50,7 +50,7 @@ const Food = () => {
             Save
           </button>
           <button className="px-4 py-2 bg-orange-500 hover:bg-orange-600 text-white rounded-md shadow-md transition" onClick={() => addToCart(food.id)}>
-            Order
+            Add to cart
           </button>
         </div>
       </div>
