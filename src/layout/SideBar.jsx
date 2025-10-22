27099f51d7144bd8bfd1ea213582936988558fcf
Tanvir Ahmmed <tanvir004006@gmail.com>
2025-10-22 21:04:19 +0600

    update

--- src/layout/SideBar.jsx
+++ src/layout/SideBar.jsx
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react'
-import { Link , useNavigate} from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 
 import { ShopContext } from '../components/Context'
@@ -7,7 +7,7 @@ import { IoMdArrowDropleftCircle } from "react-icons/io";
 import { MdFoodBank } from "react-icons/md";
 
 const SideBar = () => {
-  const { sidebar, setSidebar, loader,setLoader, setUser } = useContext(ShopContext)
+  const { sidebar, setSidebar, loader, setLoader, setUser } = useContext(ShopContext)
   const hideBar = () => {
     setSidebar(!sidebar)
   }
@@ -46,21 +46,23 @@ const SideBar = () => {
 
         <a href="/" className='text-2xl font-semibold flex flex-row items-center justify-center gap-2 '><span className='text-orange-500 text-2xl'><MdFoodBank /></span> Flavor</a>
         <div className='w-auto flex flex-col gap-3'>
+         
 
+            <Link to='/' className='px-4 hover:scale-105 ' onClick={hideBar}>Home</Link>
+            <Link to='/menu' className='px-4 hover:scale-105 ' onClick={hideBar}>Menu</Link>
+            <Link to='/reserve' className='px-4 hover:scale-105 ' onClick={hideBar}>Reserve</Link>
+            <Link to='/gallery' className='px-4 hover:scale-105 ' onClick={hideBar}>Gallery</Link>
+            <Link to='/about' className='px-4 hover:scale-105 ' onClick={hideBar}>About</Link>
+            <Link to='/contact' className='px-4 hover:scale-105 ' onClick={hideBar}>Contact</Link>
+            <Link to='/saved' className='px-4 hover:scale-105 ' onClick={hideBar}>Saved</Link>
+            <Link to='/orders' className='px-4 hover:scale-105 ' onClick={hideBar}>Orders</Link>
+            <Link to='/review' className='px-4 hover:scale-105 ' onClick={hideBar}>Review</Link>
+         
         </div>
         {!loader ? <>
-
-          <Link to='/' className='px-4 hover:scale-105 ' onClick={hideBar}>Home</Link>
-          <Link to='/menu' className='px-4 hover:scale-105 ' onClick={hideBar}>Menu</Link>
           <Link to='/cart' className='px-4 hover:scale-105 ' onClick={hideBar}>Cart</Link>
-          <Link to='/reserve' className='px-4 hover:scale-105 ' onClick={hideBar}>Reserve</Link>
-          <Link to='/gallery' className='px-4 hover:scale-105 ' onClick={hideBar}>Gallery</Link>
-          <Link to='/about' className='px-4 hover:scale-105 ' onClick={hideBar}>About</Link>
-          <Link to='/contact' className='px-4 hover:scale-105 ' onClick={hideBar}>Contact</Link>
-          <Link to='/saved' className='px-4 hover:scale-105 ' onClick={hideBar}>Saved</Link>
-          <Link to='/orders' className='px-4 hover:scale-105 ' onClick={hideBar}>Orders</Link>
-          <Link to='/review' className='px-4 hover:scale-105 ' onClick={hideBar}>Review</Link>
           <Link to='/profile' className='px-4 hover:scale-105 ' onClick={hideBar}>Profile</Link>
+
         </>
           :
           < >
